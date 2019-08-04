540535181edccb620a341396a6cb445d57e33f22
Ruoyu Wang <karen_lr@msn.com>
2019-08-03 22:41:22 -0700

    post detailed view

--- Insta/urls.py
+++ Insta/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-
-from Insta.views import HelloWorld
+from Insta.views import PostListView, PostDetailedView
 
 urlpatterns = [
-    path('', HelloWorld.as_view(), name='helloworld')
+    path('posts/', PostListView.as_view(), name = 'posts'),
+    path('post/<int:pk>', PostDetailedView.as_view(), name = 'post_detail'),
 ]
--- Insta/views.py
+++ Insta/views.py
@@ -1,5 +1,14 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from .models import Post
 
 # Create your views here.
 class HelloWorld(TemplateView):
     template_name = 'test.html'
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'test.html'
+
+class PostDetailedView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
